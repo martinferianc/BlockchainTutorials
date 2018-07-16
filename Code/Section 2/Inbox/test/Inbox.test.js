b32dbaf26284cd4a96627e3fd9beca032464b856
Martin Ferianc <ferianc.martin@gmail.com>
2018-07-16 09:35:35 +0100

    [Section 2] Lecture 30

--- Code/Section 2/Inbox/test/Inbox.test.js
+++ Code/Section 2/Inbox/test/Inbox.test.js
@@ -3,8 +3,73 @@ const assert = require('assert');
 //Ganache is only for a local test network
 const ganache = require('ganache-cli');
 
-// Constructor for web3
+//Constructor for web3
 const Web3 = require('web3');
 
 //Creates an instance and connect it to the local test network
 const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile')
+
+let accounts;
+let inbox;
+
+beforeEach(async ()=> {
+    //Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+            //Teaches web3 about what methods an
+            // Inbox contract has
+    inbox = await new web3.eth.Contract(JSON.parse(interface)) //<- inbox represents the contract directly
+
+            // Tells web3 that we want to
+            // deploy a new contract
+        .deploy({data:bytecode, arguments:['Hi there!']})
+
+            // Instructs web3 to send out a
+            // transaction that creates this
+            // contract
+        .send({from: accounts[0], gas:'1000000'})
+});
+
+describe('Index',()=>{
+    it('Address assigned', ()=>{
+        
+        assert.ok(inbox.options.address);
+    });
+})
+
+
+
+
+/*
+class Car {
+
+    park(){
+        return 'stopped';
+    }
+    drive(){
+        return 'vroom';
+    }
+    constructor() {
+
+    }
+}
+
+let car;
+
+// Setup the test framework
+beforeEach(()=>{
+    car = new Car();
+});
+
+//Groupt tests together
+describe('Car', ()=>{
+    //Individual test cases
+    it('TestPark', ()=>{
+        assert.equal(car.park(),'stopped');
+    });
+    it('TestDrive', ()=>{
+        assert.equal(car.drive(),'vroom');
+    });
+});
+*/
--- Notes/Section_2.md
+++ Notes/Section_2.md
@@ -22,3 +22,17 @@ const source = fs.readFileSync(inboxPath, 'utf8');
 //Compile it!
 module.exports = solc.compile(source, 1).contracts[':Inbox'];
 ```
+
+## Testing through Mocha platform
+
+Couple of functions to keep in mind:
+
+_it_: Run a test and make an assertion
+
+_describe_: Group together _it_ functions
+
+_beforeEach_ Execute some general setup code
+
+## web3 with Contracts
+
+We can use web3 to create a deploy a contract or use it as an interface to interact with contracts that have already been deployed and created. For intraction we do not need the bytecode, but we need the address of a deployed contract. 
